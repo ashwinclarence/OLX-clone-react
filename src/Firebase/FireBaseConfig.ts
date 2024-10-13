b45474f9338f38b50814d5eef68090d0dafe870e
ASHWIN <meashwinclarence@gmail.com>
2024-10-13 20:37:16 +0530

    fea: ui updated

--- src/Firebase/FireBaseConfig.ts
+++ src/Firebase/FireBaseConfig.ts
@@ -4,25 +4,15 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { collection, doc, getFirestore } from "firebase/firestore";
 import {  getStorage } from "firebase/storage";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD1jumtNSFd0TzJpNR1l4AE4mX3QsQIP-o",
-  authDomain: "olx-clone-8f231.firebaseapp.com",
-  projectId: "olx-clone-8f231",
-  storageBucket: "olx-clone-8f231.appspot.com",
-  messagingSenderId: "945058189246",
-  appId: "1:945058189246:web:96e45e6c53515d5a5873ef",
-  measurementId: "G-03XVW3TFV4",
+  apiKey: import.meta.env.VITE_FIREBASE_API,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
 };
-// const firebaseConfig1 = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API,
-//   authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-//   projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-//   appId: import.meta.env.VITE_FIREBASE_APPID,
-//   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
-// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
